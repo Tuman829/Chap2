d20553d65838375cd7515214c638cc980108eed3
Boris.Tuman <Boris.tuman@gmail.com>
2013-05-06 13:04:35 -0400

    chap2 first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chap2::Application.config.secret_token = '3e8391c80affeb4b605fa0ecafe910bc7551bfa343acb2a8dffdce22a8479c2e983b31b86106bdf87f5a36b87a1a65031ff326451818317a8667c2b169814735'
